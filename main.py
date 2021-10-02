f6b114160af547413e4a83c8f5f3c6fae868ba30
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-10-02 07:51:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# Practice: Writing While Loops #
+
+#a
+x = 1
+while x < 6:
+  print(x)
+  x += 1
+
+print()
+
+#b
+x = 2
+while x <12:
+  print(x)
+  x += 3
+
+print()
+
+#c
+x = -10
+while x < 1:
+  print(x, end=" ")
+  x += 2
+
+print()
+print()
+
+#d 
+x = 0
+while x < 4:
+  print(4 * "*")
+  x += 1
+
+print()
+
+for x in range(4):
+  print("****")
+
+print()
+
+string = "Art!"
+i = 0
+while i < len(string):
+  print(string,i)
+  i += 1
+print()
+
+for x in range(1):
+  print("Art")
